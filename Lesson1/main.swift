eaa8c5d4d32a25265ebf40ee8b60347187357b5c
leonif <leonif1@gmail.com>
2021-03-02 20:34:21 +0200

    my first project for lesson 1

--- Lesson1/main.swift
+++ Lesson1/main.swift
@@ -7,5 +7,64 @@
 
 import Foundation
 
-print("Hello, World!")
+//// Array
+//var dreamTeam: [String] = [
+//    "Igor Fedyav", // 0
+//    "Vladimmir Gabulov", // 1
+//    "Viktor Vasin" // 2
+//]
+//
+//print(dreamTeam[2])
+//
+//
+//// Dictionary
+//var priceList: [String: Double] = [
+//    "orange" : 34.67,
+//    "apple": 45.89,
+//    "Orange": 0
+//]
+//
+//
+//let r = priceList["Orange2"]
+//
+//if let safeR = r {
+//    print(safeR)
+//} else {
+//    print("No values")
+//}
+
+
+
+//var t: Bool? = true
+
+
+
+
+//// Set
+//var list1: Set<String> = [
+//    "orange",
+//    "apple",
+//    "blue"
+//]
+//
+//var list2: Set<String> = [
+//    "orange",
+//    "red",
+//]
+//
+//let result = list2.isSubset(of: list1)
+//print(result)
+
+var teamEmblema = Set<String>()
+
+
+teamEmblema.insert("Chelsy")
+teamEmblema.insert("Chelsy")
+teamEmblema.insert("Liverpool")
+teamEmblema.insert("Chelsy")
+teamEmblema.insert("Shaktar")
+
+print(teamEmblema)
+
+let arr = Array(teamEmblema)
 
